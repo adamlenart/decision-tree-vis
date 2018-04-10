8bb443147e50c2b56e9184f9b6fec069196a91f3
tingwenbao <haliotins@gmail.com>
2018-04-09 20:03:03 -0700

    add leaf name to decision path

--- displayr-sankey/js/decision-paths.js
+++ displayr-sankey/js/decision-paths.js
@@ -65,11 +65,11 @@ function drawDecisionPathTable(data) {
         .data(d3.values(data.decision_paths))
         .enter()
         .append('tr')
-    
+
     var rowIndex = 0;
     var cells = rows.selectAll('td')
         .data(function (row) {
-            return colnames.map(function (colname,i) {  
+            return colnames.map(function (colname,i) {
                 if(colname === 'rank') {
                     rowIndex++;
                 }
@@ -87,8 +87,8 @@ function drawDecisionPathTable(data) {
                 return d.rownum;
             }
             if(d.column === 'terminal_node') {
-                console.log(data.leaf_values);
-                console.log(d.value);
+                //console.log(data.leaf_values);
+                //console.log(d.value);
                 return d.value + ': [' + data.leaf_values[d.value] + "]";
             }
             return d.value;
